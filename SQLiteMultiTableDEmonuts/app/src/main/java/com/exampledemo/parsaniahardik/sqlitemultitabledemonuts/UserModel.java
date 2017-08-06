e71643b9f03ae780014457d5ad37fa3066d9894d
stjais <satyamjaiswal97@gmail.com>
2017-08-06 11:55:36 +0530

    add

---
+++ SQLiteMultiTableDEmonuts/app/src/main/java/com/exampledemo/parsaniahardik/sqlitemultitabledemonuts/UserModel.java
@@ -0,0 +1,42 @@
+package com.exampledemo.parsaniahardik.sqlitemultitabledemonuts;
+
+import java.io.Serializable;
+
+
+public class UserModel implements Serializable{
+
+    private String name, hobby, city;
+    private int id;
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getHobby() {
+        return hobby;
+    }
+
+    public void setHobby(String hobby) {
+        this.hobby = hobby;
+    }
+}
